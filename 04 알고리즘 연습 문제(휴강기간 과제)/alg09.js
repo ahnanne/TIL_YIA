811af76d01489fa55a6b432e4d4b2a1945761696
ahnanne <54733637+ahnanne@users.noreply.github.com>
2021-01-12 11:05:51 +0900

    알고리즘 연습 문제 9.정수제곱근 판별하기

---
+++ 04 알고리즘 연습 문제(휴강기간 과제)/alg09.js
@@ -0,0 +1,14 @@
+// 정수제곱근 판별하기
+function nextSqaure(n){
+  if (Number.isInteger(n ** .5)) return ((n ** .5) + 1) ** 2;
+  else return 'no';
+}
+
+console.log(nextSqaure());    // no
+console.log(nextSqaure(0));   // 1
+console.log(nextSqaure(1));   // 4
+console.log(nextSqaure(2));   // no
+console.log(nextSqaure(3));   // no
+console.log(nextSqaure(121)); // 144
+console.log(nextSqaure(165)); // no
+console.log(nextSqaure(400)); // 441
