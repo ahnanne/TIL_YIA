f8703aef7d36d3fd8ed3ea81f379863c646bca8b
ahnanne <54733637+ahnanne@users.noreply.github.com>
2021-01-18 10:19:57 +0900

    변수 이름 수정

--- 04 알고리즘 연습 문제(휴강기간 과제)/alg15.js
+++ 04 알고리즘 연습 문제(휴강기간 과제)/alg15.js
@@ -1,15 +1,15 @@
 // 직사각형이 되는 나머지 좌표 구하기
 function getRestCoordinate(array) {
   // array 평탄화
-  const newArray = array.flat();
+  const flatArray = array.flat();
 
   // 새로운 좌표를 반환하기 위한 빈 배열 생성
   const coord = [];
 
   // 각 숫자는 총 2회 등장해야 하며, 1회 등장한 숫자 2개로 좌표를 만들면 됨.
   // 이때, 먼저 등장한 숫자가 x축이 되어야 함. 즉 새로운 배열에 먼저 push되어야 함.
-  newArray.forEach(v => {
-    if (!newArray.includes(v, newArray.indexOf(v) + 1)) coord.push(v);
+  flatArray.forEach(v => {
+    if (!flatArray.includes(v, flatArray.indexOf(v) + 1)) coord.push(v);
   });
 
   return coord;
