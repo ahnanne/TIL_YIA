ab71e5f57d8f5e4eebc405ff1639891dffd61d7d
ahnanne <54733637+ahnanne@users.noreply.github.com>
2021-01-18 19:02:10 +0900

    알고리즘 연습 문제 22.두 정수 사이의 합

---
+++ 04 알고리즘 연습 문제(휴강기간 과제)/alg22.js
@@ -0,0 +1,34 @@
+// 두 정수 사이의 합
+function adder(x, y) {
+  // 🍕 x = y일 경우
+  if (x === y) return x;
+
+  // 🍕 x > y일 경우
+  else if (x > y) {
+    const arr = [];
+    for (let i = 0; i <= x - y; i++) arr[i] = y + i;
+
+    // 합 구하기
+    let sum = 0;
+    arr.reduce((_, num) => sum += num, 0);
+
+    return sum;
+  }
+
+  // 🍕 x < y일 경우
+  else {
+    const arr = [];
+    for (let i = 0; i <= y - x; i++) arr[i] = x + i;
+
+    // 합 구하기
+    let sum = 0;
+    arr.reduce((_, num) => sum += num, 0);
+
+    return sum;
+  }
+}
+
+console.log(adder(3, 5)); // 12
+console.log(adder(5, 3)); // 12
+console.log(adder(3, 3)); // 3
+console.log(adder(10, 1)); // 55
