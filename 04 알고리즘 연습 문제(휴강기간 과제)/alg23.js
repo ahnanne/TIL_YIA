516ad94179e8dcb619d2871380ad27f7b381572f
ahnanne <54733637+ahnanne@users.noreply.github.com>
2021-01-19 18:37:00 +0900

    알고리즘 연습 문제 23.배열의 인접한 요소곱 중 가장 큰 값 구하기

--- 04 알고리즘 연습 문제(휴강기간 과제)/alg23.js
+++ 04 알고리즘 연습 문제(휴강기간 과제)/alg23.js
@@ -1,1 +1,25 @@
 // 배열의 인접한 요소곱 중 가장 큰 값 구하기
+function adjacentElementsProduct(arr) {
+  // 인접한 요소곱 값을 담아 둘 새로운 배열 생성
+  const resArr = [];
+
+  // 요소곱 구해서 resArr에 집어넣기
+  let res = 0;
+  arr.reduce((_, cur, index) => {
+    if (!arr[index + 1]) return;
+    else {
+      res = cur * arr[index + 1];
+      resArr.push(res);
+    }
+  }, 1);
+
+  // resArr 중 가장 큰 값 구하기
+  let max = -Infinity;
+  resArr.reduce((_, cur) => {
+    if (cur > max) max = cur
+  }, 0);
+
+  return max;
+}
+
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3])); // 21
